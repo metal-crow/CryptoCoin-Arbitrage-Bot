e14d99a224bf8da981ae30feeeb9a36922547b12
metal-crow <frankcrisanti@aol.com>
2015-03-11 00:24:46 -0400

    work begins again! finally!

--- README.md
+++ README.md
@@ -9,16 +9,23 @@ only do this is price difference > transfer costs
   
 http://bitcoincharts.com/markets/currency/USD.html  
   
-list of possible markets  
+hitbtc is usually highest
+  
+list of possible markets(must accept usd for btc, debit cards, allow usa users, and give btc in <3 hours)  
 | market   	| Buy btc with usd                                                                         	         | Sell btc      |
-|----------	|:--------------------------------------------------------------------------------------------------:|--------------:|
-| coinbase 	| <b>YES</b> (accepts bank tranfer, very slow, can use credit card)                                	 | <b>YES</b>    |
-| cex.io   	| <b>NO</b> (does not accept bank tranfer from usa, usa blocked for payment card transactions)     	 | <b>YES</b>    |
-| hitbtc   	| <b>???</b> (website is shit, cant verify)                                                     	 | <b>YES</b>    |
-| bitstamp 	| <b>NO</b> (INTERNATIONAL bank transfers only, 0.1% deposit fee on our side (minimum fee = $15) ) 	 | <b>YES</b>    |
-| bitbay   	| <b>Currently no<b/> Uses OKPay, which CURRENTLY doesnt support USA                               	 | <b>YES</b>    |
-| btc-e  	| <b>Currently no<b/> Uses OKPay, which CURRENTLY doesnt support USA                                 | <b>YES</b>    |
-| circle  	| <b>???</b> are'nt responding to my verification requests                                           | <b>NO</b>     |
-| coincafe 	| <b>YES</b> NEED TO VERIFY                                                                          | <b>??</b>     |
-| trucoin 	| <b>NO</b> Not in usa                                                                               | <b>??</b>     |
-| coin.mx 	| <b>SCAM</b> www.reddit.com/r/Bitcoin/comments/2j7v1j/do_not_use_coinmx_the_website_is_a_scam/      | <b>NO</b>     |
+|---------------|:------------------------------------------------------------------------------------------------------:|--------------:|
+| coinbase   	| <b>YES</b> (accepts bank tranfer, very slow, can use credit card but not debit)                     	 | <b>YES</b>    |
+| cex.io        | <b>NO</b> (does not accept bank tranfer from usa, usa blocked for payment card transactions)     	 | <b>YES</b>    |
+| hitbtc   	    | <b>???</b> (website is shit, cant verify)                                                     	 | <b>YES</b>    |
+| bitstamp      | <b>NO</b> (international bank transfers only, 0.1% deposit fee on our side (minimum fee = $15) ) 	 | <b>YES</b>    |
+| coin.mx       | <b>SCAM</b> www.reddit.com/r/Bitcoin/comments/2j7v1j/do_not_use_coinmx_the_website_is_a_scam/          | <b>NO</b>     |
+| Bitodex.com   | <b>SCAM?</b> paypal isnt working                                                                       | <b>NO</b>     |
+| Bitin.co      | paypal isnt working
+| btcquick      | Website down for 3 months now
+| coincorner    | unable to process applications from your country at present
+| coincafe      | <b>YES</b> credit/debit requires 2 days                                                               | <b>??</b>     |
+| bitbay     	| <b>Currently no<b/> Uses OKPay, which CURRENTLY doesnt support USA                               	 | <b>YES</b>    |
+| btc-e     	| <b>Currently no<b/> Uses OKPay, which CURRENTLY doesnt support USA                                     | <b>YES</b>    |
+| 247exchange   | <b>Currently no</b>usa support in 2015-16                                                              | <b>NO</b>     |
+| circle        | <b>YES</b>                                                                                             | <b>NO</b>     |
+| trucoin       | <b>YES</b> WAITING FOR VERIFY                                                                          | <b>??</b>     |
--- src/Main.java
+++ src/Main.java
@@ -28,193 +28,56 @@ public class Main {
         //find way to run headless
         //no point making unified method because each website has unique tags
         
-        //log into btce
-        WebDriver btce = new FirefoxDriver();
-        btce.get("https://btc-e.com");
-        WebElement login=btce.findElement(By.id("login"));
-        login.findElement(By.id("email")).sendKeys(loginInfo.get(0).getValue0());
-        login.findElement(By.id("password")).sendKeys(loginInfo.get(0).getValue1());
+        //log into circle(usd to btc)
+        WebDriver circle = new FirefoxDriver();
+        circle.get("https://www.circle.com/signin");
+        WebElement login=circle.findElement(By.name("form"));
+        login.findElement(By.name("email")).sendKeys(loginInfo.get(0).getValue0());
+        login.findElement(By.name("pass")).sendKeys(loginInfo.get(0).getValue1());
         login.submit();
         boolean loggedin=false;
         while(!loggedin){
-            loggedin = btce.findElements(By.className("profile")).size() > 0;
+            loggedin = circle.findElements(By.className("page-overview-actions")).size() > 0;
         }
-        System.out.println("logged in btce");
+        System.out.println("logged into circle");
         
-        //log in to btistamp
-        WebDriver bitstamp = new FirefoxDriver();
-        bitstamp.get("https://www.bitstamp.net/account/login/");
-        WebElement login2=bitstamp.findElement(By.id("login_form"));
-        login2.findElement(By.id("id_username")).sendKeys(loginInfo.get(1).getValue0());
-        login2.findElement(By.id("id_password")).sendKeys(loginInfo.get(1).getValue1());
+        //log in to hitbtc
+        WebDriver hitbtc = new FirefoxDriver();
+        hitbtc.get("https://hitbtc.com/sso/signin");
+        WebElement login2=hitbtc.findElement(By.id("form_index_signin"));
+        login2.findElement(By.name("username")).sendKeys(loginInfo.get(1).getValue0());
+        login2.findElement(By.name("password")).sendKeys(loginInfo.get(1).getValue1());
         login2.submit();
         boolean loggedin2=false;
         while(!loggedin2){
-            loggedin2 = bitstamp.findElement(By.xpath("//div[@class=\"container\"]/div[@class=\"right\"]/ul")).getText().contains("LOGOUT");
+            loggedin2 = hitbtc.findElements(By.className("menuBar_userSignedIn ")).size() > 0;
         }
-        bitstamp.get("https://www.bitstamp.net/market/tradeview/");
-        System.out.println("logged in bitstamp");
+        System.out.println("logged into hitbtc");
         
         while(true){
             try{
-                //ordered from low to high
-                //store price/coin, # of coins, and usd cost
-                ArrayList<Triplet<Double, Double, Double>> lowestsellers=new ArrayList<Triplet<Double, Double, Double>>();
+                //check if the price at circle, minus transaction fees, is greater than the bid price at hitbtc, minus transaction fees
                 
-                //ordered from high to low
-                ArrayList<Triplet<Double, Double, Double>> highestbuyers= new ArrayList<Triplet<Double, Double, Double>>();
+                //conversion rate is autoupdated, so no worry about refreshing
+                //TODO do i have to get this every time to reget the changed string, or can i just use .gettext on the existing var?
+                WebElement circleprice_elem=circle.findElement(By.className("page-overview-rate"));
+                String circleprice_string=circleprice_elem.getText();
                 
-                int websitesellat=0;//defaults to 0, btce
-                int websitebuyat=0;
+                double circleprice=Double.valueOf(circleprice_string.substring(circleprice_string.indexOf("$")));
+                //circle processing fee is 0.029%, rounded up to nearest cent
+                circleprice=Math.round(circleprice*102.9)/100.0;
                 
-                //------------btce------------
+                //hitbtc's current selling price is auto updated
+                WebElement hitbtcsellingprice_elem=hitbtc.findElement(By.className("form_createOrder__offer"));
+                double hitbtcsellingprice=Double.valueOf(hitbtcsellingprice_elem.getText());
+                //TODO hitbtc's withdraw fees are a Flat rate $9
                 
-                //first child is the lowest
-                List<WebElement> btcesellers=btce.findElements(By.xpath("//div[@id=\"orders-s-list\"]/*/*/*/tr[@class=\"order\"]"));
-                for(WebElement seller:btcesellers){
-                    //NOTE: using strings is 5x faster than findElement
-                    String data=seller.getText();
-                    int fspace=data.indexOf(" ");
-                    int sspace=data.indexOf(" ", fspace+1);
-
-                    lowestsellers.add(
-                            Triplet.with(
-                                    Double.valueOf(data.substring(0, fspace)), 
-                                    Double.valueOf(data.substring(fspace, sspace)), 
-                                    Double.valueOf(data.substring(sspace))
-                                    )
-                            );
+                //buy!
+                if(circleprice<hitbtcsellingprice){
+                    //must buy btc rounded to 0.01, otherwise i lose money to hitbtc rounding limits
+                
+                    //TODO circle requires google authenticator to buy, figure out how to use
                 }
-                //element [2] is the amount being sold, [3] is the total cost
-                
-                //first child is highest
-                List<WebElement> btcebuyers=btce.findElements(By.xpath("//div[@id=\"orders-b-list\"]/*/*/*/tr[@class=\"order\"]"));
-                for(WebElement buyer:btcebuyers){
-                    String data=buyer.getText();
-                    int fspace=data.indexOf(" ");
-                    int sspace=data.indexOf(" ", fspace+1);
-                    highestbuyers.add(
-                            Triplet.with(
-                                    Double.valueOf(data.substring(0, fspace)), 
-                                    Double.valueOf(data.substring(fspace, sspace)), 
-                                    Double.valueOf(data.substring(sspace))
-                                    )
-                            );
-                }
-                //element [2] is the amount being bought, [3] is the total profit
-                
-                
-                //---------------------------------
-                //--------------bitstamp------------
-                
-                //first child is highest (these people are buying, so we sell to them)
-                WebElement bitstamp_highest_buyer=bitstamp.findElement(By.xpath("//tbody[@id=\"bids\"]/tr[1]"));
-
-                double highest_Buyer_price=Double.valueOf(bitstamp_highest_buyer.findElement(By.xpath(".//td[@class=\"price\"]")).getText());
-                if(highest_Buyer_price>lowestsellers.get(0).getValue0()){
-                    websitesellat=1;//selling website is now bitstamp
-                    //since these people are buying for higher, use them
-                    List<WebElement> bitstampbuyers=bitstamp.findElements(By.xpath("//tbody[@id=\"bids\"]/tr"));
-                    highestbuyers.clear();
-                    for(WebElement buyer:bitstampbuyers){
-                        String data=buyer.getText();
-                        //data could change while being read
-                        if(!data.equals("") && data!=null){
-                            int fspace=data.indexOf(" ");
-                            int sspace=data.indexOf(" ", fspace+1);
-                            
-                            highestbuyers.add(
-                                    Triplet.with(
-                                            Double.valueOf(data.substring(sspace)), 
-                                            Double.valueOf(data.substring(fspace, sspace)), 
-                                            Double.valueOf(data.substring(0, fspace))
-                                            )
-                                    );
-                        }
-                    }
-                }
-                
-                //first child is lowest
-                WebElement bitstamp_lowest_seller=bitstamp.findElement(By.xpath("//tbody[@id=\"asks\"]/tr[1]"));
-                
-                double lowest_seller_price=Double.valueOf(bitstamp_lowest_seller.findElement(By.xpath(".//td[@class=\"price\"]")).getText());
-                if(lowest_seller_price<lowestsellers.get(0).getValue0()){
-                    websitebuyat=1;//buying website is now bitstamp
-                    //since these people are selling for lower, use them
-                    List<WebElement> bitstampsellers=bitstamp.findElements(By.xpath("//tbody[@id=\"asks\"]/tr"));
-                    lowestsellers.clear();
-                    for(WebElement seller:bitstampsellers){
-                        String data=seller.getText();
-                        //data could change while being read
-                        if(!data.equals("") && data!=null){
-                            int fspace=data.indexOf(" ");
-                            int sspace=data.indexOf(" ", fspace+1);
-                            
-                            lowestsellers.add(
-                                    Triplet.with(
-                                            Double.valueOf(data.substring(sspace)), 
-                                            Double.valueOf(data.substring(fspace, sspace)), 
-                                            Double.valueOf(data.substring(0, fspace))
-                                            )
-                                    );
-                        }
-                    }
-                }
-                
-                //---------------------------------
-                //----------check if arbitrage possible-----------
-                int index_to_buy_from=0;
-                int index_to_sell_to=0;
-                boolean doit=false;
-                
-                while(index_to_buy_from<lowestsellers.size() && !doit){
-                    Triplet<Double, Double, Double> seller=lowestsellers.get(index_to_buy_from);
-                    //find seller to buy from in p1 who is selling less # of coins than the buyer in p2 who is buying the max # of coins.
-                    //they also must be selling at lower $/coin than the buyer is buying at
-                    //TODO i can further maxamize profit if i search highest buyers as well for a match
-                    if(seller.getValue0()<highestbuyers.get(index_to_sell_to).getValue0() && seller.getValue1()<highestbuyers.get(index_to_sell_to).getValue1()){
-                        doit=true;
-                    }
-                    else{
-                        index_to_buy_from++;
-                    }
-                }
-                
-                //----------------------------------------
-                //perform arbitrage
-                DecimalFormat df=new DecimalFormat("#.0000");
-                
-                /*double how_much_usd_id_buy=0;
-                if(websitebuyat==0){
-                    String text=lowestseller.getText();
-                    how_much_usd_id_buy=Double.valueOf(text.substring(text.lastIndexOf(" ")));
-                    System.out.println("Buy $"+df.format(how_much_usd_id_buy)+" from btce");
-                }else if(websitebuyat==1){
-                    String text=lowestseller.getText();
-                    how_much_usd_id_buy=Double.valueOf(text.substring(text.lastIndexOf(" ")));
-                    System.out.println("Buy $"+df.format(how_much_usd_id_buy)+" from bitstamp");
-                }
-                double how_much_usd_id_sell=0;
-                if(websitesellat==0){
-                    String text=highestbuyer.getText();
-                    how_much_usd_id_sell=Double.valueOf(text.substring(text.lastIndexOf(" ")));
-                    System.out.println("Sell $"+df.format(how_much_usd_id_sell)+" to btce");
-                }
-                else if(websitesellat==1){
-                    String text=highestbuyer.getText();
-                    how_much_usd_id_sell=Double.valueOf(text.substring(0,text.indexOf(" ")));
-                    System.out.println("Sell $"+df.format(how_much_usd_id_sell)+" to bitstamp");
-                }*/
-                
-                if(doit){
-                    System.out.println("Buy "
-                                            +lowestsellers.get(index_to_buy_from).getValue1()+" coins at "
-                                            +websitebuyat+" for $"+lowestsellers.get(index_to_buy_from).getValue0()+"/coin "
-                                    + " and sell "
-                                            +highestbuyers.get(index_to_sell_to).getValue1()+" coins at "
-                                            +websitesellat+" for $"+highestbuyers.get(index_to_sell_to).getValue0()+"/coin");
-                }
-                
                 //rate limit myself
                 Thread.sleep(5000);
             }catch(StaleElementReferenceException e){
